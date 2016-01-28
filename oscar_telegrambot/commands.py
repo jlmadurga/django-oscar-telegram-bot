9d52470c679ec3bc4c218599bec30f7ca813d2fc
Juan Madurga <jlmadurga@gmail.com>
2016-01-28 18:37:30 +0100

    bot handlers

--- oscar_telegrambot/commands.py
+++ oscar_telegrambot/commands.py
@@ -1,11 +1,11 @@
 from oscar_telegrambot.commands_views import StartView, HelpView, UnknownView, \
     CategoryListDetailView, ProductListDetailView, OrdersCommandView
+from telegrambot.handlers import command, unknown_command
 
-
-commandspatterns = [('start', StartView.as_command_view()),
-                    ('help', HelpView.as_command_view()),
-                    ('categories', CategoryListDetailView.as_command_view()),
-                    ('products', ProductListDetailView.as_command_view()),
-                    ('orders', OrdersCommandView.as_command_view()),
-                    (None, UnknownView.as_command_view())
-                    ]
+bothandlers = [command('start', StartView.as_command_view()),
+               command('help', HelpView.as_command_view()),
+               command('categories', CategoryListDetailView.as_command_view()),
+               command('products', ProductListDetailView.as_command_view()),
+               command('orders', OrdersCommandView.as_command_view()),
+               unknown_command(UnknownView.as_command_view())
+               ]
